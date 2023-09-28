b455a298ac978c7acad1697ee1fb3e308c30584b
caisilus <serangor@gmail.com>
2023-09-28 13:02:48 +0300

    Add id and plus operator to lexer

--- src/polynomLexer.js
+++ src/polynomLexer.js
@@ -1,5 +1,9 @@
-function isNumber(str) {
-  return str.match(/\d+/) !== null
+function isDigit(c) {
+  return /\d/.test(c)
+}
+
+function isLetter(c) {
+  return /[a-z]/.test(c)
 }
 
 class PolynomLexer {
@@ -9,30 +13,88 @@ class PolynomLexer {
   }
   
   nextLexem() {
+    this.skipDummy()
+
     const numberToken = this.getNumber()
     if (numberToken !== null) {
       return numberToken
+    }
+
+    const identifier = this.getIdentifier()
+    if (identifier !== null) {
+      return identifier
+    }
+
+    const operation = this.getOperation()
+    if (operation !== null) {
+      return operation
+    }
+
+    return null
+  }
+
+  skipDummy() {
+    while (this.currentChar().trim().length === 0) {
+      this.nextChar()
     }
   }
 
   getNumber() {
     const startingIndex = this.currentIndex
 
-    while (!this.inputEnded() && isNumber(this.currentChar())) {
-      this.nextChar()
-    } 
+    const number = this.takeWhile((c) => isDigit(c))
 
-    if (this.currentIndex == startingIndex) {
+    if (this.currentIndex === startingIndex) {
       return null
     }
 
     return {
       type: "number",
       position: startingIndex,
-      value: this.input.slice(startingIndex, this.currentIndex)
+      value: number
     }
   }
  
+  getIdentifier() {
+    const startingIndex = this.currentIndex
+
+    const id = this.takeWhile((c) => isLetter(c))
+    
+    if (id == null) {
+      return null
+    }
+    
+    return {
+      type: "identifier",
+      position: startingIndex,
+      value: id 
+    }
+  }
+  getOperation() {
+    switch (this.currentChar()) {
+      case "+": return {
+        type: "plus operator", 
+        position: this.currentIndex++,
+        value: "+"
+      } 
+      default: return null
+    }
+  }
+
+  takeWhile(condition) {
+    const startingIndex = this.currentIndex
+
+    const cond = !this.inputEnded() && condition(this.currentChar())
+    const test = this
+    while (!this.inputEnded() && condition(this.currentChar())) {
+      this.nextChar()
+    } 
+
+    const slice = this.input.slice(startingIndex, this.currentIndex)
+
+    return slice.length > 0 ? slice : null
+  }
+
   inputEnded() {
     return this.currentIndex === this.input.length
   }
